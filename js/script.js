dfdb14b804c73c34ffd0c0159e5acd9ea20d2f10
chiaraxs <chiaracardinale92@gmail.com>
2021-12-21 15:27:35 +0100

    added alert with random numbers

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Dopo 30 secondi i numeri vengono nascosti nell’html e l’utente deve inserire, 
 // uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// 1. creo funzione per creazione numeri random da restituire in array vuoto 'numbers'
+function getRandom(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+// 2. creo array vuoto per numeri random
+let numbers = [];
+
+// 3. creo ciclo while per riempire array vuoto con numeri da 1 a 50
+// e fissare la lunghezza massima di numbers a 5 
+// li pusho randomicamente nell'alert
+while(numbers.length < 5) {
+    let randomNumber = getRandom(1,50);
+    if (!numbers.includes(randomNumber)){
+        numbers.push(randomNumber)
+    }
+}
+
+console.log(numbers) // debug controllo in console
+
+// 4. creo alert che mostra numeri random
+alert('Inizia il gioco! Memorizza i seguenti numeri:' + ' ' + numbers);
