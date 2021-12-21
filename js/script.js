2d23317e725e41258ed6f06b8e5611b5d79d23ec
chiaraxs <chiaracardinale92@gmail.com>
2021-12-21 17:24:34 +0100

    added setTimeout

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-let outputHtml = document.querySelector('.hide-numbers');
+const outputHtml = document.querySelector('.output');
 
 // 1. creo funzione per creazione numeri random da restituire in array vuoto 'numbers'
 function getRandom(min, max) {
@@ -29,18 +29,20 @@ console.log(numbers) // debug controllo in console
 // 4. creo alert che mostra numeri random
 alert('Inizia il gioco! Memorizza i seguenti numeri:' + ' ' + numbers);
 
-// // 5. setto il timer a 3 s per far sparire alert da sostituire con div .hide-numbers
-// // in cui stampo i numeri da memorizzare. 
-// // dopo 3 s
+// // 5. setto il timer a 1 s per far apparire i numeri alla chiusura dell'alert
+// e farli stampare nel div 'output' 
 
-// setTimeout (hideNumbers, 3000);
+setTimeout (function () {
+    outputHtml.innerHTML = numbers; 
+}, 1000);
 
-// function hideNumbers() {
-    
-    
-// }
-
-
-
+// 6. setto timer a 3 s per far sparire i numeri e far ricomparire il div 'output' vuoto
+setTimeout (function() {
+    outputHtml.innerHTML = '';
+}, 3000);
 
 
+// 7. aggiungo prompt per inserimento numeri utente dopo 7s
+setTimeout (function (){
+    prompt('Inserisci qui i numeri!');
+}, 7000);
