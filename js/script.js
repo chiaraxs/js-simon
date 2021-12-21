7890d7ef3ba2cc4f55a45d5b3c5fdbd8439e5da5
chiaraxs <chiaracardinale92@gmail.com>
2021-12-21 12:53:33 +0100

    creation + first setup

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri vengono nascosti nell’html e l’utente deve inserire, 
+// uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
